c90204c09d6abaf6c45f2eb3e854504626e188f7
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-27 20:54:34 +0300

    Zadacha64

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Console.WriteLine("Введите число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Натуральные числа в промежутке {numberM} и {numberN} кратные трём = " );
+Numberall(numberM, numberN);
+
+void Numberall(int numberA, int numberN)
+{
+    
+    if (numberA >= numberN) return;
+    
+    else if (numberA % 3 == 0)
+    {
+        Console.Write( numberA + "; " );
+        
+    }
+    numberA++;
+    Numberall (numberA, numberN);
+
+}
